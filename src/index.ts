b2866c5ccf01496024a7e478fbd138e80458a923
Rafael Silva <rafaelsilvar221@gmail.com>
2024-10-03 20:23:12 -0300

    FEAT: Config index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('start')
+import express from  'express';
+import dotenv from  'dotenv';
+
+dotenv.config()
+
+const PORT =  process.env.PORT || 8080;
+
+const app = express()
+app.get('/', (request, response) => {
+response.send('server up 2')
+})
+app.listen(PORT, () => {
+  console.log('server  is running on port', PORT);
+
+})
